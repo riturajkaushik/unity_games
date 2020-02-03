4d9b03266e604d7807f9b64f0742b8881c8d6f9f
Rituraj Kaushik <rituraj.kaushik95@gmail.com>
2020-02-03 18:12:39 +0100

    Adds coin animation

---
+++ basic_obstacle_game/Assets/coin_animation.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class coin_animation : MonoBehaviour
+{
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Rotate (0, 0, 1);       
+    }
+}
