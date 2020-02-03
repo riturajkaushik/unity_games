703fa17ca54aa9e9619ef7d134cc9d1d6fa1a29b
Rituraj Kaushik <rituraj.kaushik95@gmail.com>
2020-02-03 15:22:23 +0100

    Initial commit

---
+++ basic_obstacle_game/Assets/follow_cube.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class follow_cube : MonoBehaviour
+{
+    public Transform cube;
+    public Vector3 offset;
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log(cube.position);
+        transform.position = cube.position + offset;
+    }
+}
