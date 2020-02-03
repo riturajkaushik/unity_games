e0b938386a349bb832aa2eac7d75e08e07985dfc
Rituraj Kaushik <rituraj.kaushik95@gmail.com>
2020-02-03 15:52:15 +0100

    ..

---
+++ README.md
@@ -0,0 +1,2 @@
+# unity_games
+Some fun projects done as a part of testing Unity for game development
---
+++ basic_obstacle_game/Assets/cube_collision.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class cube_collision : MonoBehaviour
+{
+    void OnCollisionEnter(Collision info)
+    {
+        // Debug.Log(info.collider.name);
+        // if(info.collider.name=="obstacle")
+        if(info.collider.tag=="Obstacle")
+            Debug.Log("Obstacle hit");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
