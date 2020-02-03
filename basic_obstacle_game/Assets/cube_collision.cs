04bf2f0f1b0301a88b0f2abd7c5e7cac411f7881
Rituraj Kaushik <rituraj.kaushik95@gmail.com>
2020-02-03 18:01:23 +0100

    scoring

--- basic_obstacle_game/Assets/cube_collision.cs
+++ basic_obstacle_game/Assets/cube_collision.cs
@@ -2,12 +2,19 @@
 
 public class cube_collision : MonoBehaviour
 {
+    static int score = 0;
     void OnCollisionEnter(Collision info)
     {
         // Debug.Log(info.collider.name);
         // if(info.collider.name=="obstacle")
         if(info.collider.tag=="Obstacle")
             Debug.Log("Obstacle hit");
+        if(info.collider.tag=="Coin")
+        {
+            score +=1;
+            Debug.Log("Coin hit. Score: " + score);
+
+        }
     }
     // Start is called before the first frame update
     void Start()
